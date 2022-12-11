c60fcd339a72ceb4871e88f7ed35250bbcaa7060
Rangga-PR <ranggaputraraharja@gmail.com>
2022-12-12 01:17:10 +0700

    add footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="flex items-center justify-center py-8">
+    <Text>Designed & Built by Rangga Putra R</Text>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import Text from "../common/Text/Text.vue";
+</script>
