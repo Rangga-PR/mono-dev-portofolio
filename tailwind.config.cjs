e1199515302753cba7926ef2178dd548e50ba916
Rangga-PR <ranggaputraraharja@gmail.com>
2022-12-11 09:41:06 +0700

    initiate project and dependencies

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffffff",
+        primaryshade: "#f7f7f7",
+        secondary: "#474747",
+        secondaryshade: "#3d3d3d",
+      },
+      fontFamily: {
+        main: ["Prompt", "sans-serif"],
+        sub: ['"IBM Plex Mono"', "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
