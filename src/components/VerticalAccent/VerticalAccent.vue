49c21ae80c8d0082e77fee697b753480310f47e5
Rangga-PR <ranggaputraraharja@gmail.com>
2022-12-11 10:54:10 +0700

    add vertical accents

---
+++ src/components/VerticalAccent/VerticalAccent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="fixedClasses" class="top-0 left-[calc(((100vw-1000px)/2)-64px)]">
+    <div class="mb-2" :class="barClasses" />
+    <Text class="break-all w-2" v-for="char in text">{{ char }}</Text>
+  </div>
+
+  <div
+    :class="fixedClasses"
+    class="bottom-0 right-[calc(((100vw-1000px)/2)-64px)]"
+  >
+    <a class="mb-4" :href="`mailto:${email}`" target="_blank">
+      <Mail w="20" h="20" />
+    </a>
+
+    <a :href="github" target="_blank">
+      <Github w="20" h="20" />
+    </a>
+
+    <div class="mt-2" :class="barClasses" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Text from "../common/Text/Text.vue";
+import Github from "../common/Icon/Github.vue";
+import Mail from "../common/Icon/Mail.vue";
+import { email, github } from "../../data";
+
+const fixedClasses =
+  "hidden lg:flex flex-col items-center fixed z-10 bg-primary dark:bg-secondary px-1";
+const barClasses = "h-72 w-1 bg-secondary dark:bg-primary";
+
+const text = ref(email);
+</script>
---
+++ src/components/common/Icon/Mail.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg
+    :width="props.w"
+    :height="props.h"
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      :class="fillClasses"
+      d="M22.5 2.5H2.5C1.125 2.5 0 3.625 0 5V20C0 21.375 1.125 22.5 2.5 22.5H22.5C23.875 22.5 25 21.375 25 20V5C25 3.625 23.875 2.5 22.5 2.5ZM22.5 20H20V9L12.5 13.75L5 9V20H2.5V5H4L12.5 10.25L21 5H22.5V20Z"
+      :fill="props.color"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = withDefaults(
+  defineProps<{ color?: string; h?: string; w?: string }>(),
+  {
+    h: "25",
+    w: "25",
+  }
+);
+
+const fillClasses = computed(() =>
+  props.color ? "" : "fill-secondary dark:fill-primary"
+);
+</script>
