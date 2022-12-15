1b05d2bf94f701e25e8401d9ba5747850e431c1b
Rangga-PR <ranggaputraraharja@gmail.com>
2022-12-16 03:15:39 +0700

    animate hero

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -1,15 +1,39 @@
 <template>
   <section
     id="about"
-    class="flex flex-col justify-center p-4 aspect-[21/9] bg-primary dark:bg-secondary"
+    class="flex flex-col justify-center p-4 bg-primary dark:bg-secondary h-96"
+    ref="heroRef"
   >
-    <Text type="h2">Hi,</Text>
-    <Text type="h1">I'am Rangga</Text>
-    <Text class="mt-4 max-w-lg">
-      I’m a software engineer specializing in building web applications. Mostly
-      I work on frontend, but i also enjoy working on backend.
-    </Text>
-    <Button class="mt-6" type="a" :href="`mailto:${email}`">CONTACT ME </Button>
+    <Transition :duration="950">
+      <div v-show="onScreen">
+        <Text type="h2" id="greeting" class="content" :class="transitionClasses"
+          >Hi,</Text
+        >
+        <Text
+          type="h1"
+          id="name"
+          class="content delay-[250ms]"
+          :class="transitionClasses"
+          >I'am Rangga</Text
+        >
+        <Text
+          class="mt-4 max-w-lg content delay-[500ms]"
+          id="caption"
+          :class="transitionClasses"
+        >
+          I’m a software engineer specializing in building web applications.
+          Mostly I work on frontend, but i also enjoy working on backend.
+        </Text>
+        <Button
+          class="mt-6 content delay-[500ms]"
+          :class="transitionClasses"
+          type="a"
+          :href="`mailto:${email}`"
+          id="contact-me"
+          >CONTACT ME
+        </Button>
+      </div>
+    </Transition>
   </section>
 </template>
 
@@ -17,4 +41,20 @@
 import Text from "../common/Text/Text.vue";
 import Button from "../common/Button/Button.vue";
 import { email } from "../../data";
+import useIsIntersecting from "../../composables/useIsIntersecting";
+import { ref } from "vue";
+
+const transitionClasses =
+  "transition-[opacity,transform] duration-500 ease-in-out ";
+
+const heroRef = ref();
+const onScreen = useIsIntersecting(heroRef, { once: true });
 </script>
+
+<style scoped>
+.v-enter-from .content,
+.v-leave-to .content {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+</style>
