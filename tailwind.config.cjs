8689fa8ecefbf70243ba7b1c22766ad28b3f977f
Rangga-PR <ranggaputraraharja@gmail.com>
2022-12-17 07:46:04 +0700

    update tailwind config to overwrite default

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,18 +3,15 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {
-      colors: {
-        primary: "#ffffff",
-        primaryshade: "#f7f7f7",
-        secondary: "#474747",
-        secondaryshade: "#3d3d3d",
-      },
-      fontFamily: {
-        main: ["Prompt", "sans-serif"],
-        sub: ['"IBM Plex Mono"', "monospace"],
-      },
+    colors: {
+      primary: "#ffffff",
+      primaryshade: "#f7f7f7",
+      secondary: "#474747",
+      secondaryshade: "#3d3d3d",
+    },
+    fontFamily: {
+      main: ["Prompt", "sans-serif"],
+      sub: ['"IBM Plex Mono"', "monospace"],
     },
   },
-  plugins: [],
 };
