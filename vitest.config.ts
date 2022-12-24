975c109d8614c86f3bc38669e334f597f0acd8c2
Rangga-PR <ranggaputraraharja@gmail.com>
2022-12-24 14:21:12 +0700

    setup vitest

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vitest/config";
+import Vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [Vue()],
+  test: {
+    environment: "jsdom",
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/public/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tailwind,astro}.config.*",
+    ],
+  },
+});
