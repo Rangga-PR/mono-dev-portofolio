13cd8b051cc487c4c64d6f04ab1def3502efeb2d
Rangga-PR <ranggaputraraharja@gmail.com>
2022-12-27 14:30:11 +0700

    add work card unit test

---
+++ src/components/WorkCard/WorkCard.test.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "vitest";
+import { mount, VueWrapper } from "@vue/test-utils";
+import WorkCard from "./WorkCard.vue";
+
+const mockData = {
+  title: "fake title",
+  desc: "fake desc",
+  url: "https://fake.com/url",
+  repo: "https://fake.com/url",
+  stacks: ["cobol"],
+};
+
+test("render work card", () => {
+  const wrapper = mount(WorkCard, {
+    propsData: { work: mockData },
+  });
+
+  const text = wrapper.text();
+
+  expect(text).toContain("fake title");
+  expect(text).toContain("fake desc");
+  expect(wrapper.find("a").attributes("href")).toContain(
+    "https://fake.com/url"
+  );
+});
